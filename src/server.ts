f82791256b9aff8a5ddf8d439632778a69dc0c37
breess457 <ripper505jack@gmail.com>
2024-05-10 16:05:54 +0700

    update port

--- src/server.ts
+++ src/server.ts
@@ -77,4 +77,4 @@ app.get("/", (req:Request, res:Response) => {
   }
 });
 
-app.listen(3000, () => console.log("Server is running..."));
+app.listen(8082, () => console.log("Server is running..."));
